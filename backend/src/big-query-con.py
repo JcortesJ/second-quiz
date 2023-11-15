ca6146201d655bfd73893c0d796fc9d642acf2a3
juan <jcortesj@unal.edu.co>
2023-11-14 22:03:09 -0500

    readBigQueryMethods

--- backend/src/big-query-con.py
+++ backend/src/big-query-con.py
@@ -8,7 +8,7 @@ def createJson(dictionary,fileName):
         # Convertir el diccionario a una cadena JSON
         json.dump(dictionary, f, indent=2)
 
-def query_by_state_Species(codigoEstado,especie):
+def standard_query(firstYear=1950,lastYear=2010,initialDiameter=0,initialHeight=0,limit=100):
 # Crea un cliente de BigQuery
     client = bigquery.Client.from_service_account_json('./my-key.json')
 # Especifica el archivo JSON de claves de API
@@ -16,28 +16,151 @@ def query_by_state_Species(codigoEstado,especie):
 # Perform a query.
     QUERY = (
     """
-    SELECT species_common_name AS tree, COUNT(species_scientific_name) AS state 
-    FROM `bigquery-public-data.usfs_fia.tree` 
-    WHERE tree_state_code=@cod_estado AND species_common_name=@n_especie GROUP BY(species_common_name) LIMIT 10
+    SELECT DISTINCT species_common_name,tree_status_code_name,tree_state_code AS state, 
+    ROUND(AVG(current_diameter),2) AS diametro,ROUND(AVG(actual_height),2) AS height , 
+    COUNT(species_scientific_name) AS number FROM `bigquery-public-data.usfs_fia.tree` 
+    WHERE tree_inventory_year>@f_year AND tree_inventory_year<@l_year AND current_diameter > @i_diameter
+    AND actual_height > @i_height 
+    GROUP BY species_common_name,tree_status_code_name,tree_state_code  LIMIT @limit;
     """)
     job_config = bigquery.QueryJobConfig(
     query_parameters=[
-        bigquery.ScalarQueryParameter("n_especie", "STRING", especie),
-        bigquery.ScalarQueryParameter("cod_estado", "INT64", codigoEstado),
+        bigquery.ScalarQueryParameter("f_year", "INT64",firstYear),
+        bigquery.ScalarQueryParameter("l_year", "INT64",lastYear),
+        bigquery.ScalarQueryParameter("i_diameter", "INT64", initialDiameter),
+        bigquery.ScalarQueryParameter("i_height", "INT64", initialHeight),
+        bigquery.ScalarQueryParameter("limit", "INT64", limit),
     ]
     )
     query_job = client.query(QUERY, job_config=job_config)  # Make an API request.
     rows = query_job.result()  # Waits for query to finish
     dictionary = dict()
+    resultList = []
     if rows.total_rows != 0:
         for row in rows:
-            #print(row.values)
+            resultList.append(row.values())
             #de tipo tupla
-            dictionary[row.values()[0]] = row.values()[1]
-    else:
-        dictionary["result"] = "NaN"
+            #dictionary[row.values()[0]] = row.values()[1]
+        dictionary["result"] = resultList
     print(len(dictionary))
     createJson(dictionary=dictionary,fileName="result")
+    
+def state_query(stateCode,firstYear=1950,lastYear=2010,initialDiameter=0,initialHeight=0,limit=100):
+# Crea un cliente de BigQuery
+    client = bigquery.Client.from_service_account_json('./my-key.json')
+# Especifica el archivo JSON de claves de API
+
+# Perform a query.
+    QUERY = (
+    """
+    SELECT species_common_name,tree_status_code_name,tree_state_code AS state,
+    ROUND(AVG(current_diameter),2) AS diametro,ROUND(AVG(actual_height),2) AS height ,
+    COUNT(species_scientific_name) AS number FROM `bigquery-public-data.usfs_fia.tree` 
+    WHERE tree_state_code=@state AND tree_inventory_year>@f_year AND tree_inventory_year<@l_year AND current_diameter > @i_diameter
+    AND actual_height > @i_height
+    GROUP BY species_common_name,tree_status_code_name,tree_state_code  LIMIT @limit;
+    """)
+    job_config = bigquery.QueryJobConfig(
+    query_parameters=[
+        bigquery.ScalarQueryParameter("f_year", "INT64",firstYear),
+        bigquery.ScalarQueryParameter("l_year", "INT64",lastYear),
+        bigquery.ScalarQueryParameter("i_diameter", "INT64", initialDiameter),
+        bigquery.ScalarQueryParameter("i_height", "INT64", initialHeight),
+        bigquery.ScalarQueryParameter("limit", "INT64", limit),
+        bigquery.ScalarQueryParameter("state", "INT64", stateCode),
+    ]
+    )
+    query_job = client.query(QUERY, job_config=job_config)  # Make an API request.
+    rows = query_job.result()  # Waits for query to finish
+    dictionary = dict()
+    resultList = []
+    if rows.total_rows != 0:
+        for row in rows:
+            resultList.append(row.values())
+            #de tipo tupla
+            #dictionary[row.values()[0]] = row.values()[1]
+        dictionary["result"] = resultList
+    print(len(dictionary))
+    createJson(dictionary=dictionary,fileName="result")
+    
+    
+def state_species_query(stateCode,speciesName,firstYear=1950,lastYear=2010,initialDiameter=0,initialHeight=0,limit=100):
+# Crea un cliente de BigQuery
+    client = bigquery.Client.from_service_account_json('./my-key.json')
+# Especifica el archivo JSON de claves de API
+
+# Perform a query.
+    QUERY = (
+    """
+    SELECT species_common_name,tree_status_code_name,tree_state_code AS state,
+    ROUND(AVG(current_diameter),2) AS diametro,ROUND(AVG(actual_height),2) AS height ,
+    COUNT(species_scientific_name) AS number FROM `bigquery-public-data.usfs_fia.tree` 
+    WHERE tree_state_code=@state AND tree_inventory_year>@f_year AND tree_inventory_year<@l_year AND current_diameter > @i_diameter
+    AND actual_height > @i_height  AND species_common_name LIKE %@species_name%
+    GROUP BY species_common_name,tree_status_code_name,tree_state_code  LIMIT @limit;
+    """)
+    job_config = bigquery.QueryJobConfig(
+    query_parameters=[
+        bigquery.ScalarQueryParameter("f_year", "INT64",firstYear),
+        bigquery.ScalarQueryParameter("l_year", "INT64",lastYear),
+        bigquery.ScalarQueryParameter("i_diameter", "INT64", initialDiameter),
+        bigquery.ScalarQueryParameter("i_height", "INT64", initialHeight),
+        bigquery.ScalarQueryParameter("limit", "INT64", limit),
+        bigquery.ScalarQueryParameter("state", "INT64", stateCode),
+        bigquery.ScalarQueryParameter("species_name", "STRING", speciesName),
+    ]
+    )
+    query_job = client.query(QUERY, job_config=job_config)  # Make an API request.
+    rows = query_job.result()  # Waits for query to finish
+    dictionary = dict()
+    resultList = []
+    if rows.total_rows != 0:
+        for row in rows:
+            resultList.append(row.values())
+            #de tipo tupla
+            #dictionary[row.values()[0]] = row.values()[1]
+        dictionary["result"] = resultList
+    print(len(dictionary))
+    createJson(dictionary=dictionary,fileName="result")
+    
+def species_query(speciesName,firstYear=1950,lastYear=2010,initialDiameter=0,initialHeight=0,limit=100):
+# Crea un cliente de BigQuery
+    client = bigquery.Client.from_service_account_json('./my-key.json')
+# Especifica el archivo JSON de claves de API
+
+# Perform a query.
+    QUERY = (
+    """
+    SELECT species_common_name,tree_status_code_name,tree_state_code AS state,
+    ROUND(AVG(current_diameter),2) AS diametro,ROUND(AVG(actual_height),2) AS height ,
+    COUNT(species_scientific_name) AS number FROM `bigquery-public-data.usfs_fia.tree` 
+    WHERE tree_inventory_year>@f_year AND tree_inventory_year<@l_year AND current_diameter > @i_diameter
+    AND actual_height > @i_height  AND species_common_name LIKE %@species_name%
+    GROUP BY species_common_name,tree_status_code_name,tree_state_code  LIMIT @limit;
+    """)
+    job_config = bigquery.QueryJobConfig(
+    query_parameters=[
+        bigquery.ScalarQueryParameter("f_year", "INT64",firstYear),
+        bigquery.ScalarQueryParameter("l_year", "INT64",lastYear),
+        bigquery.ScalarQueryParameter("i_diameter", "INT64", initialDiameter),
+        bigquery.ScalarQueryParameter("i_height", "INT64", initialHeight),
+        bigquery.ScalarQueryParameter("limit", "INT64", limit),
+        bigquery.ScalarQueryParameter("species_name", "STRING", speciesName),
+    ]
+    )
+    query_job = client.query(QUERY, job_config=job_config)  # Make an API request.
+    rows = query_job.result()  # Waits for query to finish
+    dictionary = dict()
+    resultList = []
+    if rows.total_rows != 0:
+        for row in rows:
+            resultList.append(row.values())
+            #de tipo tupla
+            #dictionary[row.values()[0]] = row.values()[1]
+        dictionary["result"] = resultList
+    print(len(dictionary))
+    createJson(dictionary=dictionary,fileName="result")  
+    
 if __name__ =='__main__':
-    query_by_state_Species(6,"California live oak")
+    state_query(6,limit=20)
     
